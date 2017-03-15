b52aea95f502dec554ceff8008b27ee0d34e9321
Lorenzo Lobba <lorenzo.lobba@moxconsulting.it>
2017-03-15 17:36:29 +0100

    indent simple

--- src/colors.c
+++ src/colors.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "colors.h"
 
 /* FOREGROUND COLORS */
@@ -25,7 +25,7 @@
 
 int print_colors()
 {
-	printf(ANSI_COLOR_BG_WHITE  ANSI_COLOR_FG_BLACK   "This text is BLACK!"   ANSI_COLOR_RESET "\n");
+	printf(ANSI_COLOR_BG_WHITE ANSI_COLOR_FG_BLACK   "This text is BLACK!"   ANSI_COLOR_RESET "\n");
 	printf(ANSI_COLOR_FG_RED     "This text is RED!"     ANSI_COLOR_RESET "\n");
 	printf(ANSI_COLOR_FG_GREEN   "This text is GREEN!"   ANSI_COLOR_RESET "\n");
 	printf(ANSI_COLOR_FG_YELLOW  "This text is YELLOW!"  ANSI_COLOR_RESET "\n");
