932a4411a243f2bc1023efcf70d64c028b9ac078
Lorenzo Lobba <lorenzo.lobba@moxconsulting.it>
2017-03-16 11:51:14 +0100

    [F] modify Makefile [F] Change name from print to sandbox
    [F] New Makefile

--- Makefile
+++ Makefile
@@ -1,10 +1,29 @@
+IDIR=include
 CC=gcc
-CFLAGS=-Iinclude
-DEPS = colors.h
-OBJ = src/print.o src/colors.o
+CFLAGS=-I$(IDIR)
 
-%.o: %.c $(DEPS)
+ODIR=build
+LDIR=../lib
+SDIR=src
+
+LIBS=-lm
+
+_DEPS = colors.h
+DEPS = $(patsubst %,$(IDIR)/%,$(_DEPS))
+
+_OBJ = colors.o sandbox.o
+OBJ = $(patsubst %,$(ODIR)/%,$(_OBJ))
+
+$(ODIR)/%.o: $(SDIR)/%.c $(DEPS)
 	$(CC) -c -o $@ $< $(CFLAGS)
 
-print: $(OBJ)
-	gcc -o $@ $^ $(CFLAGS)
+sandbox: $(OBJ)
+	gcc -o $(ODIR)/$@ $^ $(CFLAGS) $(LIBS)
+
+.PHONY: clean
+
+clean:
+	rm -f $(ODIR)/*.o $(ODIR)/sandbox *~ core $(INCDIR)/*~
+
+test:
+	./build/sandbox
--- src/colors.c
+++ src/colors.c
@@ -1,28 +1,6 @@
 #include <stdio.h>
 #include "colors.h"
 
-/* FOREGROUND COLORS */
-#define ANSI_COLOR_FG_BLACK   "\x1b[30m"
-#define ANSI_COLOR_FG_RED     "\x1b[31m"
-#define ANSI_COLOR_FG_GREEN   "\x1b[32m"
-#define ANSI_COLOR_FG_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_FG_BLUE    "\x1b[34m"
-#define ANSI_COLOR_FG_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_FG_CYAN    "\x1b[36m"
-#define ANSI_COLOR_FG_WHITE   "\x1b[37m"
-
-/* BACKGROUND COLORS */
-#define ANSI_COLOR_BG_BLACK   "\x1b[40m"
-#define ANSI_COLOR_BG_RED     "\x1b[41m"
-#define ANSI_COLOR_BG_GREEN   "\x1b[42m"
-#define ANSI_COLOR_BG_YELLOW  "\x1b[43m"
-#define ANSI_COLOR_BG_BLUE    "\x1b[44m"
-#define ANSI_COLOR_BG_MAGENTA "\x1b[45m"
-#define ANSI_COLOR_BG_CYAN    "\x1b[46m"
-#define ANSI_COLOR_BG_WHITE   "\x1b[47m"
-
-#define ANSI_COLOR_RESET   "\x1b[0m"
-
 int print_colors()
 {
 	printf(ANSI_COLOR_BG_WHITE ANSI_COLOR_FG_BLACK   "This text is BLACK!"   ANSI_COLOR_RESET "\n");
